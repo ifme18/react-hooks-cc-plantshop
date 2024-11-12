d9c41c291d97e235fbeb64a1e326a22e1a795293
ifme18 <mainakenken188@gmail.com>
2024-11-12 16:35:49 +0300

    atleast

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,47 +1,25 @@
 import React, { useState } from "react";
 
-function PlantCard({ plant, onToggleSoldOut, onUpdatePrice, onDelete }) {
-  const [price, setPrice] = useState(plant.price)
-  const [isEditing, setIsEditing] = useState(false)
+function PlantCard({ plant }) {
+  
+  const [isInStock, setIsInStock] = useState(true);
 
-  function handlePriceChange(e) {
-    setPrice(e.target.value)
-  }
-
-  function handlePriceSubmit(e) {
-    e.preventDefault();
-    console.log("submitting price")
-    onUpdatePrice(plant.id, parseFloat(price))
-    setIsEditing(false)
-  }
+  const handleClick = () => {
+    setIsInStock(!isInStock);
+  };
 
   return (
-    <li className="card" data-testid="plant-item">
-      <img src={plant.image} alt={plant.name} />
+    <li data-testid="plant-item" key={plant.id} className="card">
+      <img alt={plant.name} src={plant.image} />
       <h4>{plant.name}</h4>
-
-      {isEditing ? (
-        <form onSubmit={handlePriceSubmit}>
-            <input type="number" step="0.01" value={price} onChange={handlePriceChange}/>
-            <button type="submit">Save</button>
-        </form>
+      <p>Price: {plant.price}</p>
+      {isInStock ? (
+        <button className="primary" onClick={handleClick}>In Stock</button>
       ) : (
-        <div>
-        <p>
-          Price: {price}{""}  
-        </p>
-        <button className="btn-edit" onClick={() => setIsEditing(true)}>Edit Price</button>
-        </div>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
-      <button className={plant.soldOut ? "" : "primary"}
-        onClick={() => onToggleSoldOut(plant.id, !plant.soldOut)}
-      >
-        {plant.soldOut ? "Out of Stock" : "In Stock"}
-      </button>
-      <button className="btn-delete" onClick={() => onDelete(plant.id)}>Remove</button>
     </li>
   );
 }
 
 export default PlantCard;
-
